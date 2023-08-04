f601b6dd7f8a5084ab2b3ed928f31a364d718373
Valdeir Frizzera <valdeirdev@hotmail.com>
2023-08-04 15:50:45 -0400

    save form contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
+
 # Create your views here.
 def create(request):
 
     if request.method == 'POST':
         print(request.POST.get('first_name'))
 
+        form = ContactForm(data=request.POST)  
+
         context = {
-            'form': ContactForm(data=request.POST)   
+            'form': form 
         }
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('contact:create')
+
+
         return render(
             request,
             'contact/create.html',
